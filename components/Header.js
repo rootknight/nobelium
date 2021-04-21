83ba426124aa690f1d8124796e12612eb41889b5
rootknight <ljmroot@gmail.com>
2021-04-21 14:23:51 +0800

    复原

--- components/Header.js
+++ components/Header.js
@@ -66,7 +66,33 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/">
             <div className="h-6">
-              <embed width="24" height="24" src="https://www.besalt.top/favicon.svg" type="image/svg+xml">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  width="24"
+                  height="24"
+                  className="fill-current text-black dark:text-white"
+                />
+                <rect width="24" height="24" fill="url(#paint0_radial)" />
+                <defs>
+                  <radialGradient
+                    id="paint0_radial"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="rotate(45) scale(39.598)"
+                  >
+                    <stop stopColor="#CFCFCF" stopOpacity="0.6" />
+                    <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
+                  </radialGradient>
+                </defs>
+              </svg>
             </div>
           </Link>
           {navBarTitle ? (
