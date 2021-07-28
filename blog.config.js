059ea636574d8907c497c5cbe26913780086e29f
rootknight <ljmroot@gmail.com>
2021-07-28 17:28:25 +0800

    Update blog.config.js
    use cusdis

--- blog.config.js
+++ blog.config.js
@@ -37,7 +37,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: 'cusidis', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: 'gitalk-comment', // The repository of store comments
       owner: 'rootknight',
